c1465ccdc2e52e099b45de540e000538ea2e338b
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-10 17:28:30 +0100

    Added `AbstractModel`

---
+++ src/Model/AbstractModel.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace RebelCode\Bookings\Model;
+
+use RebelCode\Bookings\Data\DataObject;
+use RebelCode\Bookings\Storage\ResourceModelInterface;
+
+/**
+ * Abstract implementation of a model.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractModel extends DataObject implements ModelInterface
+{
+    /**
+     * The default ID field name.
+     *
+     * @since [*next-version*]
+     */
+    const DEFAULT_ID_FIELD_NAME = 'id';
+
+    /**
+     * The ID field name.
+     *
+     * @since [*next-version*]
+     *
+     * @var string
+     */
+    protected $idFieldName = null;
+
+    /**
+     * The resource model instance.
+     *
+     * @since [*next-version*]
+     *
+     * @var ResourceModelInterface
+     */
+    protected $resourceModel;
+
+    /**
+     * {@inheritdoc}
+     * 
+     * @since [*next-version*]
+     */
+    public function getIdFieldName()
+    {
+        return is_null($this->idFieldName)
+            ? static::DEFAULT_ID_FIELD_NAME
+            : $this->idFieldName;
+    }
+
+    /**
+     * Sets the ID field name.
+     * 
+     * @since [*next-version*]
+     *
+     * @param string $idFieldName
+     *
+     * @return $this
+     */
+    protected function _setIdFieldName($idFieldName)
+    {
+        $this->idFieldName = $idFieldName;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getId()
+    {
+        return $this->getData($this->getIdFieldName());
+    }
+
+    /**
+     * {@inheritdoc}
+     * 
+     * @since [*next-version*]
+     */
+    public function getResourceModel()
+    {
+        return $this->resourceModel;
+    }
+
+    /**
+     * {@inheritdoc}
+     * 
+     * @since [*next-version*]
+     *
+     * @return $this This instance.
+     */
+    protected function _setResourceModel(ResourceModelInterface $resourceModel)
+    {
+        $this->resourceModel = $resourceModel;
+
+        return $this;
+    }
+}
---
+++ test/unit/Model/AbstractModelTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace RebelCode\Bookings\Test\Model;
+
+use RebelCode\Bookings\Model\AbstractModel;
+use Xpmock\TestCase;
+
+/**
+ * Description of AbstractModelTest.
+ *
+ * @since [*next-version*]
+ */
+class AbstractModelTest extends TestCase
+{
+    /**
+     * Creates the test subject instance.
+     *
+     * @since [*next-version*]
+     *
+     * @return AbstractModel
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock('RebelCode\\Bookings\\Model\\AbstractModel')
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests the ID field name getter with the field name left as default.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetFieldNameDefault()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertEquals(AbstractModel::DEFAULT_ID_FIELD_NAME, $subject->getIdFieldName());
+    }
+
+    /**
+     * Tests the ID field name getter with a preset value.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetFieldNamePresetValue()
+    {
+        $subject = $this->createInstance();
+
+        $this->reflect($subject)->idFieldName = 'test';
+
+        $this->assertEquals('test', $subject->getIdFieldName());
+    }
+
+    /**
+     * Tests the ID getter method with the field name left as default.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetIdDefaultFieldName()
+    {
+        $subject = $this->createInstance();
+
+        $this->reflect($subject)->data = array(
+            'id' => 'test123',
+        );
+
+        $this->assertEquals('test123', $subject->getId());
+    }
+
+    /**
+     * Tests the ID getter method with a pre-set field name.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetIdPresetFieldName()
+    {
+        $subject = $this->createInstance();
+
+        $reflection              = $this->reflect($subject);
+        $reflection->idFieldName = 'some_key';
+        $reflection->data        = array(
+            'some_key' => 'test123',
+            'id'       => 54321,
+        );
+
+        $this->assertEquals('test123', $subject->getId());
+    }
+
+    /**
+     * Tests the resource model getter method when no resource model has been set.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetResourceModelNull()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertEquals(null, $subject->getResourceModel());
+    }
+
+    /**
+     * Tests the resource model getter method with a preset instance.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetResourceModelPresetInstance()
+    {
+        $subject = $this->createInstance();
+
+        $rm                                     = $this->getMockForAbstractClass('RebelCode\\Bookings\\Storage\\ResourceModelInterface');
+        $this->reflect($subject)->resourceModel = $rm;
+
+        $this->assertEquals($rm, $subject->getResourceModel());
+    }
+
+    /**
+     * Tests the resource model setter method.
+     *
+     * @since [*next-version*]
+     */
+    public function testSetResourceModel()
+    {
+        $subject = $this->createInstance();
+
+        $rm = $this->getMockForAbstractClass('RebelCode\\Bookings\\Storage\\ResourceModelInterface');
+        $this->reflect($subject)->_setResourceModel($rm);
+
+        $this->assertEquals($rm, $this->reflect($subject)->resourceModel);
+    }
+}
