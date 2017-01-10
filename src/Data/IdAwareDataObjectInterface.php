15b010617fff7f0932a06e1e8c4cf63f5f7be784
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-10 12:44:02 +0100

    Added `IdAwareDataObjectInterface`

---
+++ src/Data/IdAwareDataObjectInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace RebelCode\Bookings\Data;
+
+/**
+ * A data object that can have an ID.
+ *
+ * @since [*next-version*]
+ */
+interface IdAwareDataObjectInterface extends IdAwareInterface, DataObjectInterface
+{
+    /**
+     * Gets the field name of the ID.
+     *
+     * @since [*next-version*]
+     *
+     * @return string
+     */
+    public function getIdFieldName();
+}
