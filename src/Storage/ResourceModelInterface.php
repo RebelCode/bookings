2e628871b8deeaba51df2d32df452de29bb5c7fa
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-10 10:42:54 +0100

    Added `ResourceModelInterface`

---
+++ src/Storage/ResourceModelInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace RebelCode\Bookings\Storage;
+
+use RebelCode\Bookings\Model\ModelInterface;
+
+/**
+ * A resource model is responsible for handling.
+ *
+ * @since [*next-version*]
+ */
+interface ResourceModelInterface
+{
+    /**
+     * Saves the model to storage.
+     *
+     * @since [*next-version*]
+     *
+     * @param ModelInterface $model The model instance.
+     */
+    public function save(ModelInterface $model);
+
+    /**
+     * Loads the model from storage.
+     *
+     * @since [*next-version*]
+     *
+     * @param ModelInterface $model The model instance.
+     */
+    public function load(ModelInterface $model);
+
+    /**
+     * Deletes the model from storage.
+     *
+     * @since [*next-version*]
+     *
+     * @param ModelInterface $model The model instance.
+     */
+    public function delete(ModelInterface $model);
+}
