f4b1fc9737176a474a95cf39e7e731c5212cbfac
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-10 12:52:32 +0100

    `ModelInterface` extends `ResourceModelAwareInterface`

--- src/Model/ModelInterface.php
+++ src/Model/ModelInterface.php
@@ -3,12 +3,13 @@
 namespace RebelCode\Bookings\Model;
 
 use RebelCode\Bookings\Data\IdAwareDataObjectInterface;
+use RebelCode\Bookings\Storage\ResourceModelAwareInterface;
 
 /**
  * Any object that can be considered a "model".
  *
  * @since [*next-version*]
  */
-interface ModelInterface extends IdAwareDataObjectInterface
+interface ModelInterface extends IdAwareDataObjectInterface, ResourceModelAwareInterface
 {
 }
