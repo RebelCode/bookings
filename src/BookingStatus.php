e6aedc1c98d6eee6974b417337f0e972105227e6
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-10 10:09:13 +0100

    Added `BookingStatus` enum

---
+++ src/BookingStatus.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RebelCode\Bookings;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * Booking status enumeration.
+ *
+ * @since [*next-version*]
+ */
+class BookingStatus extends Enum
+{
+    const CONFIRMED = 'confirmed';
+    const PENDING   = 'pending';
+    const COMPLETED = 'completed';
+    const IN_CART   = 'in_cart';
+    const CANCELLED = 'cancelled';
+    const REJECTED  = 'rejected';
+    const NO_SHOW   = 'no_show';
+    const PAID      = 'paid';
+    const UNPAID    = 'unpaid';
+    const DRAFT     = 'draft';
+}
