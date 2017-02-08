a51ae640c18384b9104b2ff162a1a8ae17abd5f1
Miguel Muscat <miguelmuscat93@gmail.com>
2017-02-08 14:42:35 +0100

    Added `RuleInterface`

---
+++ src/Model/Availability/RuleInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RebelCode\Bookings\Model\Availability;
+
+use \RebelCode\Bookings\Validation\BookingValidatorInterface;
+
+/**
+ * Something that defines a rule that restricts availability.
+ *
+ * @since [*next-version*]
+ */
+interface RuleInterface extends BookingValidatorInterface
+{
+}
