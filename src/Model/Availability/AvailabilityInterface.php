c55d27b1a803681f466b4f94943b04a5f2ffaaa4
Miguel Muscat <miguelmuscat93@gmail.com>
2017-02-08 14:42:35 +0100

    Added `AvailabilityInterface`

---
+++ src/Model/Availability/AvailabilityInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RebelCode\Bookings\Model\Availability;
+
+use \RebelCode\Bookings\Validation\BookingValidatorInterface;
+
+/**
+ * Something that represents an availability.
+ *
+ * @since [*next-version*]
+ */
+interface AvailabilityInterface extends BookingValidatorInterface
+{
+}
