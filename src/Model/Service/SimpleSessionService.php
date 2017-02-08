1be646fdef59f10115477862ebf2aac5de06ca34
Miguel Muscat <miguelmuscat93@gmail.com>
2017-02-08 14:42:35 +0100

    Added `SimpleSessionService` model class

---
+++ src/Model/Service/SimpleSessionService.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace RebelCode\Bookings\Model\Service;
+
+use \RebelCode\Bookings\Framework\Model\GenericModel;
+use \RebelCode\Diary\DateTime\PeriodInterface;
+
+/**
+ * A simple implementation of a service that does not have period-specific configuration.
+ *
+ * The configuration for this service (session length, price, etc.) apply to all sessions,
+ * regardless of time period or other parameters such as number of people, location, etc.
+ *
+ * @since [*next-version*]
+ */
+class SimpleSessionService extends GenericModel implements SessionServiceInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getMinSessions(PeriodInterface $period, array $args = array())
+    {
+        return $this->getData('min_sessions', 1);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getMaxSessions(PeriodInterface $period, array $args = array())
+    {
+        return $this->getData('max_sessions', 1);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getPrice(PeriodInterface $period, array $args = array())
+    {
+        $duration     = $period->getDuration();
+        $numSessions  = $duration / $this->getSessionLength($period, $args);
+        $fNumSessions = (isset($args['floor']) && $args['floor'])
+            ? floor($numSessions)
+            : $numSessions;
+
+        return $fNumSessions * $this->getSessionPrice($period, $args);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getSessionLength(PeriodInterface $period, array $args = array())
+    {
+        return $this->getData('session_length', 3600);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getAvailability()
+    {
+        return $this->getData('availability');
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getSessionPrice(PeriodInterface $period, array $args = array())
+    {
+        return $this->getData('session_price');
+    }
+}
