683862676ce6ab9025545e297ce621872bf50bad
Miguel Muscat <miguelmuscat93@gmail.com>
2017-02-08 17:00:06 +0100

    Added `BookingContext`

---
+++ src/Expression/BookingContext.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RebelCode\Bookings\Expression;
+
+use \Dhii\Espresso\Context\RawContext;
+
+/**
+ * An expression context for a booking instance.
+ *
+ * @since [*next-version*]
+ */
+class BookingContext extends RawContext
+{
+}
