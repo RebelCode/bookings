99bc63f915bcb3c743920d48a2ed78415418d550
Miguel Muscat <miguelmuscat93@gmail.com>
2017-02-12 18:14:11 +0100

    `RuleInterface` now extends `LogicalExpressionInterface`

--- src/Model/Availability/RuleInterface.php
+++ src/Model/Availability/RuleInterface.php
@@ -2,13 +2,13 @@
 
 namespace RebelCode\Bookings\Model\Availability;
 
-use \Dhii\Expression\ExpressionInterface;
+use \Dhii\Expression\LogicalExpressionInterface;
 
 /**
  * Something that defines a rule that restricts availability.
  *
  * @since [*next-version*]
  */
-interface RuleInterface extends ExpressionInterface
+interface RuleInterface extends LogicalExpressionInterface
 {
 }
